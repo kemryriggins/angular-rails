ed1498651ecdbc8c6c620f3653a80b8f87b72adb
kemryriggins <98kariggins@gmail.com>
2018-02-05 14:10:33 -0700

    Added the custom card containers and images to the cards. Cards have been imported to the documents section

--- freelance-camp-fe/app/documents/documents.component.js
+++ freelance-camp-fe/app/documents/documents.component.js
@@ -16,21 +16,21 @@ var DocumentsComponent = /** @class */ (function () {
                 description: 'asdfasdfasdf asdfasdf',
                 file_url: 'http://google.com',
                 updated_at: '11/11/16',
-                image_url: 'http://google.com'
+                image_url: 'https://media.gettyimages.com/videos/binary-code-tunnel-video-id463210692?s=640x640'
             },
             {
                 title: 'My Second Doc',
                 description: 'asdfasdfasdf asdfasdf',
                 file_url: 'http://google.com',
                 updated_at: '11/11/16',
-                image_url: 'http://google.com'
+                image_url: 'http://codecondo.com/wp-content/uploads/2014/04/5-Coding-Challenges-to-Help-You-Train-Your-Brain.jpg'
             },
             {
                 title: 'My Last Doc',
                 description: 'asdfasdfasdf asdfasdf',
                 file_url: 'http://google.com',
                 updated_at: '11/11/16',
-                image_url: 'http://google.com'
+                image_url: 'https://webrobots.io/wp-content/uploads/2016/11/javascript-coding-1000x576.jpg'
             }
         ];
     }
