46664fdf4d638aa3c4d823c76a1302f0399f3b96
kemryriggins <98kariggins@gmail.com>
2018-02-08 11:45:27 -0700

    added the ability to send emails from the rails microservice

--- freelance_camp_proposal/app/controllers/proposals_controller.rb
+++ freelance_camp_proposal/app/controllers/proposals_controller.rb
@@ -18,6 +18,7 @@ class ProposalsController < ApplicationController
     @proposal = Proposal.new(proposal_params)
 
     if @proposal.save
+      ProposalMailer.email(@proposal_params).deliver_later
       render json: @proposal, status: :created, location: @proposal
     else
       render json: @proposal.errors, status: :unprocessable_entity
